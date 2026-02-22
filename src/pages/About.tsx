86b9014c1d67e2ef336313ef31cd00f0b3c2cae7
Juan David <juan14gordillo@gmail.com>
2026-02-22 17:50:12 -0500

    Adición de texto en las pagians iniciales solo texto

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,6 +3,10 @@ export default function About() {
 		<div style={{ padding: '1rem' }}>
 			<h1>About Page</h1>
 			<p>This is a React app running on Cloudflare Workers.</p>
+			<div style={{ padding: '1rem' }}>
+			<h1>Sobre TKD</h1>
+			<p>Esto es un cambio para el deporte</p>
+		</div>
 		</div>
 	);
 }
